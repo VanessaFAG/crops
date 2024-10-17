4ae8ae1e0f088301146200d494f8ea5db9f3f995
Vane F. <vanessaprincss.arreo@gmail.com>
2024-10-17 08:13:46 -0600

    Clase Oct 15, corregido

--- admin/views/login/index.php
+++ admin/views/login/index.php
@@ -7,31 +7,31 @@
 
         <div class="px-5 ms-xl-4">
           <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-          <span class="h1 fw-bold mb-0">Logo</span>
+          <span class="h1 fw-bold mb-0"><img src="../image/jingliu.png" style="width: 20%;">Invernaderos</span>
         </div>
 
         <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
 
           <form style="width: 23rem;" method="post" action="login.php?accion=login">
 
-            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log In</h3>
 
             <div data-mdb-input-init class="form-outline mb-4">
               <input type="email" id="form2Example18" class="form-control form-control-lg" name="data[correo]"/>
-              <label class="form-label" for="form2Example18">Email address</label>
+              <label class="form-label" for="form2Example18">Correo electronico</label>
             </div>
 
             <div data-mdb-input-init class="form-outline mb-4">
               <input type="password" id="form2Example28" class="form-control form-control-lg" name="data[password]"/>
-              <label class="form-label" for="form2Example28">Password</label>
+              <label class="form-label" for="form2Example28">Contraseña</label>
             </div>
 
             <div class="pt-1 mb-4">
               <button data-mdb-button-init data-mdb-ripple-init class="btn btn-info btn-lg btn-block" type="submit" value="Entrar al sistema" name="Enviar">Login</button>
             </div>
 
-            <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
-            <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+            <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">¿Olvido su contraseña?</a></p>
+            <p>¿No tienes cuenta? <a href="#!" class="link-info">Restrate aqui</a></p>
 
           </form>
 
