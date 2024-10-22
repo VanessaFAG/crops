c463424053e2c29f34d9aad175b69c6c195fcbb5
GUSTAVO RAMIREZ MIRELES <gusramireles@hotmail.com>
2024-10-22 16:17:03 -0600

    validacion de contraseñas

--- admin/views/usuario/form.php
+++ admin/views/usuario/form.php
@@ -5,15 +5,29 @@ require('views/header/header_administrador.php');?>
 <form method="post" 
 action="usuario.php?accion=<?php if($accion == "crear"):echo('nuevo'); else:echo('actualizar&id='.$id);endif;?>">
 <div class="mb-3">
-    <label for="exampleUsuario" class="form-label">Correo del usuario</label>
-    <input type="text" name="data[correo]" placeholder="Escribe aquí el correo" class="form-control" 
+    <label for="correo" class="form-label">Correo del usuario</label>
+    <input id="correo" type="text" name="data[correo]" placeholder="Escribe aquí el correo" class="form-control" 
     value="<?php if(isset($usuarios['correo'])):echo($usuarios['correo']);endif;?>" />
 </div>
 
 <div class="mb-3">
-    <label for="exampleUsuario" class="form-label">Contraseña</label>
-    <input type="password" name="data[password]" placeholder="Escribe aquí la constraseña" class="form-control" 
-    value="<?php if(isset($usuarios['password'])):echo($usuarios['password']);endif;?>" />
+    <label for="password" class="form-label">Contraseña</label>
+    <input id="password" type="password" name="data[password]" placeholder="Escribe aquí la constraseña" class="form-control" 
+    value="<?php if(isset($usuarios['password'])):echo($usuarios['password']);endif;?>" disabled>
+    <div class="row">
+    <div class="col-2">
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="resetPassword">
+            <label class="form-check-label" for="resetPassword">Modificar contraseña</label>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="showPassword">
+            <label class="form-check-label" for="showPassword">Mostrar contraseña</label>
+        </div>
+    </div>
+    </div>
 </div>
 <p class="fs-5">Rol del usuario</p>
 <div class="roles">
@@ -29,4 +43,5 @@ action="usuario.php?accion=<?php if($accion == "crear"):echo('nuevo'); else:echo
     <input type="submit" name="data[enviar]" value="Guardar" class="btn btn-outline-info"
     />
 </form>
+<script src="../js/formScript.js"></script>
 <?php require('views/footer.php');?>
